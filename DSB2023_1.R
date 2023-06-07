6598704f97678dfa4c015e09d1dd4a03ef8e4e41
MickunasM <m.mickunas@outlook.com>
2023-06-07 22:21:23 +0100

    Website Creation

---
+++ DSB2023_1.R
@@ -0,0 +1,11 @@
+install.packages("blogdown")
+library(blogdown)
+
+blogdown::install_hugo()
+hugo_version()
+
+blogdown::new_site(theme = "MarcusVirg/forty", 
+                   sample = TRUE, 
+                   theme_example = TRUE,            
+                   empty_dirs = TRUE,            
+                   to_yaml = TRUE)
